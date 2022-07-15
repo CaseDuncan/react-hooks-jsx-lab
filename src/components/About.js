13f4ca24a163aa6f9c6f700bf218871b73653ea5
devdan <oluochdancun19@gmail.com>
2022-07-15 06:50:46 +0300

    jsx

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2 style={{ color: "firebrick" }}>About Me</h2>
+    <p>lorem20</p>
+    <img src={image} alt="I made this"/>
+  </div>;
 }
 
 export default About;
